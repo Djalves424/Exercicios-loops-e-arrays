2468ec9a4496b579bbd12ea70bfd40ce4fb8fb9d
Diego Alves <djalves424@gmail.com>
2023-03-16 23:35:50 -0300

    Exercicios propostos Loops e Arrays

---
+++ src/Exercicios/loops/Ex5_Tabuada.java
@@ -0,0 +1,17 @@
+package Exercicios.loops;
+
+import java.util.Scanner;
+
+public class Ex5_Tabuada {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Tabuada: ");
+        int tabuada = scan.nextInt();
+
+        System.out.println("Tabuada de " + tabuada);
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(tabuada + " X " + i + " = " + (tabuada*i));
+        }
+    }
+}
